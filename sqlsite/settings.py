6c8d498cb6e5f7d66a8f04cf0dc60ab83331cbc9
lazikos94 <lazikos94@gmail.com>
2019-10-19 17:58:35 +0300

    added statics to the sample app

--- sqlsite/settings.py
+++ sqlsite/settings.py
@@ -122,8 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-"""
+
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'pages/statics'),
+    os.path.join(BASE_DIR, 'sample/statics'),
 ]
-"""
