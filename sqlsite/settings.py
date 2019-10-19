e63df4a16ad91755357eec347e1bc49ff699c19b
lazikos94 <lazikos94@gmail.com>
2019-10-19 18:07:45 +0300

    css changes

--- sqlsite/settings.py
+++ sqlsite/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME':'SQLdb' ,
         'USER':'postgres',
-        'PASSWORD':'********',
+        'PASSWORD':'r3xulhrt',
         'PORT':'5432'
     }
 }
